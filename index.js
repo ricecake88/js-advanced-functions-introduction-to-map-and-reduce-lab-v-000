5c23caa5ec8150db308d247a9c79c89f392b4605
Grace Shih <vocalise@gmail.com>
2020-02-04 05:54:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  for(let i = 0; i < sourceArray.length; i++) {
+    newArray.push(~sourceArray[i] + 1);
+  }
+  return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i]*2);
+  }
+  return newArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i]*sourceArray[i]);
+  }
+  return newArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+  let total = startingPoint;
+  for (let i = 0; i < sourceArray.length; i++) {
+    total += sourceArray[i];
+  }
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let falseFlag = false;
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i] === false) {
+      falseFlag = true;
+    }
+  }
+  if (falseFlag)
+    return false
+  else
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i] === true) {
+      return true;
+    }
+  }
+  return false;
+}
+
